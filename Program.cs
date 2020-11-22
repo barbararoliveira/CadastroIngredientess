b67f77ef81de8c7efacc9ac0ee6c19b6c2183fed
Bárbara Oliveira <babyr.oliveira@gmail.com>
2020-11-22 02:49:46 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using cadastroingredientess.db;
+
+namespace cadastroingredientess
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string ingrediente; 
+            int tipo;
+
+            Console.WriteLine("Olá usuário, digite o nome do novo ingrediente:");
+            ingrediente = Console.ReadLine();
+            Console.WriteLine("Qual o tipo (1, 2 ou 3)?");
+            tipo = Convert.ToInt32(Console.ReadLine());
+
+            using (var db = new hamburgueriaContext())
+            {
+                var novoIngrediente = new Ingrediente
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Nome = ingrediente,
+                    TipoIngredienteId = tipo,
+                };
+
+                db.Ingrediente.Add(novoIngrediente);
+                db.SaveChanges();
+            }    
+
+            Console.WriteLine("Ingrediente incluso. Pressione qualquer tecla para finalizar:");
+            Console.ReadKey();
+        }
+    }
+}
